ff577dded8df62e2ff8d34b8cbae5d7bf66f9c68
Sanjeev Kumar <sanjeev@outlook.in>
2025-09-14 23:20:31 +0100

    updated tokenizer.py to show decoding text from tensor and added requriements.txt file

--- practice_torch/tokenizer.py
+++ practice_torch/tokenizer.py
@@ -11,10 +11,25 @@ sentence = "Apple is looking at buying U.K. startup for $1 billion."
 # This includes adding special tokens like [CLS] and [SEP]
 encoded = tokenizer(sentence, return_tensors="pt")
 
-# Step 5: Extract token IDs and tokens
+# Step 5a: Extract token IDs and tokens
 token_ids = encoded["input_ids"][0]         # Tensor of token IDs
 tokens = tokenizer.convert_ids_to_tokens(token_ids)  # Human-readable tokens
 
-# Step 6: Print results for clarity
+# Step 6a: Print results for clarity
 print("Tokens:", tokens)
 print("Token IDs:", token_ids.tolist())
+
+# Step 5b: Reverse mapping — decode token IDs back to text
+decoded_text = tokenizer.decode(token_ids, skip_special_tokens=True)
+
+# Step 6b: Print results
+print("Token IDs:", token_ids.tolist())
+print("Decoded Text:", decoded_text)
+
+# output result of prints
+"""
+Tokens: ['[CLS]', 'Apple', 'is', 'looking', 'at', 'buying', 'U.K.', 'start', '##up', 'for', '$', '1', 'billion', '.', '[SEP]']
+Token IDs: [101, 7302, 1110, 1702, 1120, 9241, 28996, 1838, 4455, 1111, 109, 122, 3775, 119, 102]
+Token IDs: [101, 7302, 1110, 1702, 1120, 9241, 28996, 1838, 4455, 1111, 109, 122, 3775, 119, 102]
+Decoded Text: Apple is looking at buying U.K. startup for $ 1 billion.
+"""
